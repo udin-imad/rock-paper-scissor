9f1766ca09374b833b6e0e0a6edcab56cdbc1bba
imaduddin <udinwebdev@gmail.com>
2023-10-04 22:12:13 +0700

    create function that play single round of the game

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,5 +1,25 @@
 function getComputerChoice() {
-    const array = ['Rock', 'Paper', 'Scissor'];
+    const array = ['ROCK', 'PAPER', 'SCISSOR'];
     const random = Math.floor(Math.random() * array.length)
     return array[random];
 };
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toUpperCase() === 'ROCK' && computerSelection === 'SCISSOR'
+        || playerSelection.toUpperCase() === 'SCISSOR' && computerSelection === 'PAPER'
+        || playerSelection.toUpperCase() === 'PAPER' && computerSelection === 'ROCK') {
+
+            return(`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection}`);
+
+        } else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection === 'PAPER'
+        || playerSelection.toUpperCase() === 'SCISSOR' && computerSelection === 'ROCK'
+        || playerSelection.toUpperCase() === 'PAPER' && computerSelection === 'SCISSOR') {
+
+            return(`You Lose! ${computerSelection} beats ${playerSelection.toUpperCase()}`);
+
+        } else {
+            return('TIED');
+        }
+};
+
+
