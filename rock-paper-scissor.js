b534aa66fde94e3fb94c814bf5f667efda208b01
imaduddin <udinwebdev@gmail.com>
2023-10-07 08:40:42 +0700

    refactoring code

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,5 +1,5 @@
-const player = document.querySelector('#player-score');
-const computer = document.querySelector('#computer-score');
+const playerWinScore = document.querySelector('#player-score');
+const computerWinScore = document.querySelector('#computer-score');
 const result = document.querySelector('#result');
 const winner = document.querySelector('#winner');
 const reset = document.querySelector('#reset');
@@ -7,63 +7,63 @@ const buttons = document.querySelectorAll('.button button');
 
 let playerScore = 0;
 let computerScore = 0;
-let gameRound = 5;
+let gameRound = 3;
 
-player.textContent = playerScore;
-computer.textContent = computerScore;
+playerWinScore.textContent = playerScore;
+computerWinScore.textContent = computerScore;
 
 function getComputerChoice() {
     const array = ['ROCK', 'PAPER', 'SCISSOR'];
-    const random = Math.floor(Math.random() * array.length)
+    const random = Math.floor(Math.random() * array.length);
     return array[random];
 };
 
-function playRound(playerSelection, computerSelection) {
+function gameplay(playerSelection, computerSelection) {
     if (playerSelection === 'ROCK' && computerSelection === 'SCISSOR'
         || playerSelection === 'SCISSOR' && computerSelection === 'PAPER'
         || playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-            playerScore++
+            ++playerScore
             return (`Computer choose ${computerSelection}\nYou Win! ${playerSelection} beats ${computerSelection}`);
 
         } else if (playerSelection === 'ROCK' && computerSelection === 'PAPER'
         || playerSelection === 'SCISSOR' && computerSelection === 'ROCK'
         || playerSelection === 'PAPER' && computerSelection === 'SCISSOR') {
-            computerScore++
+            ++computerScore
             return (`Computer choose ${computerSelection}\nYou Lose! ${computerSelection} beats ${playerSelection}`);
 
         } else {
             return (`Computer choose ${computerSelection} you choose ${playerSelection} TIED`);
-        }
+        };
 };
 
-function score() {
-    if (gameRound <= 0 && playerScore > computerScore) {
+function printScore() {
+    if (gameRound === 0 && playerScore > computerScore) {
         winner.textContent = 'YOU WIN!!!';
-    } else if (gameRound <= 0 && computerScore > playerScore) {
+    } else if (gameRound === 0 && computerScore > playerScore) {
         winner.textContent = 'YOU LOSE!!!';
-    } else if (gameRound <= 0 && computerScore === playerScore) {
+    } else if (gameRound === 0 && computerScore === playerScore) {
         winner.textContent = 'TIED';
     };
 };
 
-function game() {
-    result.textContent = playRound(playerSelection, getComputerChoice());
-    player.textContent = playerScore;
-    computer.textContent = computerScore; 
+function getScore() {
+    result.textContent = gameplay(playerSelection, getComputerChoice());
+    playerWinScore.textContent = playerScore;
+    computerWinScore.textContent = computerScore; 
+    printScore();
 };
 
-function round() {
+function startGame() {
     gameRound--;
-    score();
-    if ( gameRound >= 0 ) {
+    if (gameRound >= 0) {
         console.log(gameRound);
-        return game();
+        return getScore();
     }   else {
         return;
-    }
-}
+    };
+};
 
 buttons.forEach((btn) => {
     playerSelection = btn.id;
-    btn.addEventListener('click', round);
+    btn.addEventListener('click', startGame);
 });
